0bfb23a714b4362e8c1bef807c62b2e6359e24dd
mingmonth <ys9922@naver.com>
2019-04-04 22:07:15 +0900

    #14 AJAX on React

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -4,44 +4,13 @@ import Movie from "./Movie";
 
 class App extends Component {
   // Render: componentWillMount() -> render() -> componentDidMount()
-
   // Update: componentWillReceiveProps() -> shouldComponentUpdate() -> componentWillUpdate() -> render() -> componentDidUpdate()
 
-  componentDidMount() {
-    setTimeout(() => {
-      this.setState({
-        movies: [
-          {
-            title: "Matrix",
-            poster:
-              "https://upload.wikimedia.org/wikipedia/en/thumb/c/c1/The_Matrix_Poster.jpg/220px-The_Matrix_Poster.jpg"
-          },
-          {
-            title: "Full Metal Jacket",
-            poster:
-              "https://upload.wikimedia.org/wikipedia/en/thumb/9/99/Full_Metal_Jacket_poster.jpg/220px-Full_Metal_Jacket_poster.jpg"
-          },
-          {
-            title: "Oldboy",
-            poster:
-              "https://upload.wikimedia.org/wikipedia/en/thumb/6/67/Oldboykoreanposter.jpg/220px-Oldboykoreanposter.jpg"
-          },
-          {
-            title: "Star Wars",
-            poster:
-              "https://upload.wikimedia.org/wikipedia/en/thumb/9/93/Star_Wars_Episode_III_Revenge_of_the_Sith_poster.jpg/220px-Star_Wars_Episode_III_Revenge_of_the_Sith_poster.jpg"
-          },
-          {
-            title: "Trainspotting",
-            poster:
-              "https://upload.wikimedia.org/wikipedia/en/thumb/7/77/Trainspottingsoundtrack.jpg/220px-Trainspottingsoundtrack.jpg"
-          }
-        ]
-      });
-    }, 2000);
-  }
-
   state = {};
+
+  componentDidMount() {
+    fetch("https://yts.am/api/v2/list_movies.json?sort_by=rating");
+  }
 
   _renderMovies = () => {
     const movies = this.state.movies.map((movie, index) => {
