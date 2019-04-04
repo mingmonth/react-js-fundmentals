1b5666bcd11ec33f54572639696b413cba6aedd8
mingmonth <ys9922@naver.com>
2019-04-04 22:57:22 +0900

    #16 Async Await

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -9,17 +9,35 @@ class App extends Component {
   state = {};
 
   componentDidMount() {
-    fetch("https://yts.am/api/v2/list_movies.json?sort_by=rating")
-      .then(response => response.json())
-      .then(json => console.log(json))
-      .catch(err => console.log(err));
+    this._getMovies();
   }
 
   _renderMovies = () => {
-    const movies = this.state.movies.map((movie, index) => {
-      return <Movie title={movie.title} poster={movie.poster} key={index} />;
+    const movies = this.state.movies.map(movie => {
+      console.log(movie);
+      return (
+        <Movie
+          title={movie.title}
+          poster={movie.medium_cover_image}
+          key={movie.id}
+        />
+      );
     });
     return movies;
+  };
+
+  _getMovies = async () => {
+    const movies = await this._callApi();
+    this.setState({
+      movies
+    });
+  };
+
+  _callApi = () => {
+    return fetch("https://yts.am/api/v2/list_movies.json?sort_by=rating")
+      .then(response => response.json())
+      .then(json => json.data.movies)
+      .catch(err => console.log(err));
   };
 
   render() {
